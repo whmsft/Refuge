cc798f02e30c1a31ff8e1ed53d571ae1055adc1c
whmsft <whmsft@outlook.com>
2023-04-12 16:08:20 +0530

    Glow!

--- scenes/Elisa.gd
+++ scenes/Elisa.gd
@@ -5,6 +5,8 @@ func _ready():
 	
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
-		self.position.x -= 64 * delta
+		self.position.x -= 64 * delta;
+		self.flip_h = false;
 	if Input.is_action_pressed("ui_right"):
-		self.position.x += 64 * delta
+		self.position.x += 64 * delta;
+		self.flip_h = true;
